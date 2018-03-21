8af1ca174d278c4822a309124c42e0795fdd7b08
Panupong Pasupat <ppasupat@stanford.edu>
2018-03-21 10:22:48 -0700

    Removed unnecessary services

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,7 @@
-const users = require('./users/users.service.js');
 const pages = require('./pages/pages.service.js');
-const assets = require('./assets/assets.service.js');
 const annotations = require('./annotations/annotations.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
-  app.configure(users);
   app.configure(pages);
-  app.configure(assets);
   app.configure(annotations);
 };
