08333c440f0eb0df8331b771004e7307129ac19a
Panupong Pasupat <ppasupat@stanford.edu>
2018-05-20 16:53:52 -0700

    Edited extract-node-texts

--- tools/extract-node-texts.py
+++ tools/extract-node-texts.py
@@ -18,7 +18,12 @@ def clean(x):
 
 
 def process(filename, args):
-    with open(os.path.join(args.indir, filename)) as fin:
+    infile = os.path.join(args.indir, filename)
+    outfile = os.path.join(args.outdir, re.sub(r'\.html$', '', filename) + '.json')
+    if os.path.exists(outfile) and args.no_clobber:
+        return
+
+    with open(infile) as fin:
         soup = BeautifulSoup(fin.read(), 'html5lib')
     xid_to_text = OrderedDict()
     for x in soup.body(True):
@@ -27,18 +32,20 @@ def process(filename, args):
             text = clean(x.get_text())
             if text:
                 xid_to_text[xid] = text
-    with open(os.path.join(args.outdir, re.sub(r'\.html$', '', filename) + '.json'), 'w') as fout:
+    with open(outfile, 'w') as fout:
         json.dump(xid_to_text, fout, indent=0, separators=(',', ': '))
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--no-clobber', action='store_true')
     parser.add_argument('indir')
     parser.add_argument('outdir')
     args = parser.parse_args()
 
     for filename in sorted(os.listdir(args.indir)):
-        print 'Processing {} ...'.format(filename)
-        process(filename, args)
+        if filename.endswith('.html'):
+            print 'Processing {} ...'.format(filename)
+            process(filename, args)
     
 
 if __name__ == '__main__':
